3973a247a6b68850516a793a050878cc6011d36a
JosArtu <mitre.garcia.jose.arturo@gmail.com>
2026-06-07 12:46:12 -0600

    Health update

--- Assets/Scripts/Game/Health/HealthController.cs
+++ Assets/Scripts/Game/Health/HealthController.cs
@@ -1,16 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HealthController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private float _currentHealth;
+
+    [SerializeField]
+    private float _maximumHealth;
+
+    public float RemainingHealthPercentage
     {
-        
+        get
+        {
+            return _currentHealth / _maximumHealth;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool IsInvincible { get; set; }
+
+    public UnityEvent OnDied;
+
+    public UnityEvent OnDamaged;
+
+    public void TakeDamage(float damageAmount)
     {
-        
+        if (_currentHealth == 0)
+        {
+            return;
+        }
+
+        if (IsInvincible)
+        {
+            return;
+        }
+
+        _currentHealth -= damageAmount;
+
+        if (_currentHealth < 0)
+        {
+            _currentHealth = 0;
+        }
+
+        if (_currentHealth == 0)
+        {
+            OnDied.Invoke();
+        }
+        else
+        {
+            OnDamaged.Invoke();
+        }
+    }
+
+    public void AddHealth(float amountToAdd)
+    {
+        if (_currentHealth == _maximumHealth)
+        {
+            return;
+        }
+
+        _currentHealth += amountToAdd;
+
+        if (_currentHealth > _maximumHealth)
+        {
+            _currentHealth = _maximumHealth;
+        }
     }
 }
